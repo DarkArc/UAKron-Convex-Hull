ec52c9cbd255b68394b1d770e46ec0c071ffc9b4
Wyatt Childers <wchilders@nearce.com>
2015-04-27 00:15:41 -0400

    Documented DependantHullState.cpp

--- HullState/DependantHullState.cpp
+++ HullState/DependantHullState.cpp
@@ -15,6 +15,34 @@
 
 #include "DependantHullState.hpp"
 
+/** Construct a new DependantHullState object.
+
+    Constructs a new DependantHullState object which
+    holds a reference to some parent HullState.
+
+    This reference is then manipulated by the additional points,
+    point delete indexs, additional lines, and line delete
+    indexs.
+
+    Delete indexs must be defined in a manor in which the
+    index n + 1 references the correct point or line
+    to remove after n has been deleted.
+
+    @param parent
+    The parent HullState.
+
+    @param remPoints
+    The indexs of points to remove.
+
+    @param addPoints
+    The points to add.
+
+    @param remLines
+    The indexs of lines to remove.
+
+    @param addLines
+    The lines to add.
+ */
 DependantHullState::DependantHullState(
   const std::shared_ptr<HullState> parent,
   const std::vector<int>& remPoints,
@@ -27,8 +55,28 @@ DependantHullState::DependantHullState(
     remLines(remLines),
     addLines(addLines) { }
 
+/** Destruct the current DependantHullState object.
+
+    Destructs the current DependantHullState object.
+    This destructor is manually defined due to
+    the use of polymorphism with this class,
+    and the need for a virtual destructor.
+ */
 DependantHullState::~DependantHullState() { }
 
+/** Retrieve the calculated points.
+
+    Retrieves the points as calculated from the
+    parent's point set, modified by the current
+    object's removal point set, as well
+    as the current object's add points set.
+
+    Points are only added after all delete indexs
+    have been exaughsted.
+
+    @returns the points which make up the hull's
+    state as of this HullState.
+ */
 std::vector<QPoint> DependantHullState::getPoints() const {
   auto parentPoints = parent->getPoints();
   for (auto& entry : remPoints) {
@@ -38,6 +86,19 @@ std::vector<QPoint> DependantHullState::getPoints() const {
   return parentPoints;
 }
 
+/** Retrieve the calculated lines.
+
+    Retrieves the lines as calculated from the
+    parent's line set, modified by the current
+    object's removal line set, as well
+    as the current object's add lines set.
+
+    Lines are only added after all delete indexs
+    have been exaughsted.
+
+    @returns the lines which make up the hull's
+    state as of this HullState.
+ */
 std::vector<QLine> DependantHullState::getLines() const {
   auto parentLines = parent->getLines();
   for (auto& entry : remLines) {
